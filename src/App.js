2deb03622fc42ab3cb4fb2293f756a563a1f2f89
Elaman <taalaibekovelaman@gmail.com>
2023-06-20 21:42:12 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
-
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 function App() {
-  const [likes, setLikes ]= useState(0)
-  const [value, setValue ]= useState('Text v inpute')
 
-  function increment () {
-    setLikes (likes + 1)
-  }
-  function decrement () {
-    setLikes (likes - 1)
-  }
+  
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <PostItem post={{id:1, title: 'JavaScript', body: 'Description'}} />
+      <PostItem post={{id:1, title: 'JavaScript', body: 'Description'}} />
+      <PostItem post={{id:1, title: 'JavaScript', body: 'Description'}} />
     </div>
   );
 }
